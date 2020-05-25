3e023d2906ae8fc1cbda86d5afe3dfb429b0198b
Norbert Bonnici <norbert@bonnici.co>
2020-05-26 00:11:53 +0200

    Added figures

--- sentiment-analysis/sentiment-desktop.py
+++ sentiment-analysis/sentiment-desktop.py
@@ -9,7 +9,6 @@ import re
 from contextlib import redirect_stderr
 import pickle
 import pandas as pd
-import numpy as np
 from collections import Counter
 import seaborn as sns
 import nltk
@@ -22,6 +21,12 @@ from nltk.tokenize.casual import TweetTokenizer
 import datetime
 import typing
 from joblib import Parallel, delayed
+import matplotlib.pyplot as plt
+from pylab import plot, ylim, xlim, show, xlabel, ylabel, grid
+from numpy import linspace, loadtxt, ones, convolve
+import numpy as np
+from wordcloud import WordCloud
+
 
 stopcorpus: typing.List = stopwords.words('english')
 
@@ -36,6 +41,13 @@ def parse_args():
         metavar="Translated Twitter Data",
         help="Select the source twitter pickled data folder",
         widget="DirChooser",
+    )
+
+    parser.add_argument(
+        "city_name",
+        metavar="City Name",
+        help="Name of city to be analysed",
+        type=str,
     )
 
     parser.add_argument(
@@ -162,6 +174,93 @@ if __name__ == "__main__":
         # Creating 8 instances of the GUI, not processing each file
         # Parallel(n_jobs=8)(delayed(data_processing)(sentiment_dir, df) for df in tqdm(files))
         # print(progress_bar_output.read())
+
+    meanArr = {"date":[], 'anger':[], 'anticipation':[], 'disgust':[], 'fear':[], 'joy':[], 'negative':[], 'positive':[], 'sadness':[], 'surprise':[], 'trust':[]}
+
+    files = []
+
+    for file in os.listdir(sentiment_dir):
+        files.append(pickle.load(open(os.path.join(sentiment_dir, file),'rb')))
+
+    for file in files:
+        date = datetime.datetime.strptime(file['created_at'].min(), '%Y-%m-%d %H:%M:%S').date()
+        
+    meanArr["date"].append(date)
+    meanArr["fear"].append(file['fear'].mean()*100)
+    meanArr["anger"].append(file['anger'].mean()*100)
+    meanArr["joy"].append(file['joy'].mean()*100)
+    meanArr["positive"].append(file['positive'].mean())
+    meanArr["negative"].append(file['negative'].mean())
+    meanArr["anticipation"].append(file['anticipation'].mean())
+    meanArr["disgust"].append(file['disgust'].mean())
+    meanArr["sadness"].append(file['sadness'].mean())
+    meanArr["surprise"].append(file['surprise'].mean())
+    meanArr["trust"].append(file['trust'].mean())
+
+    plt.figure(figsize=(20,8))
+    plt.title(f'Change in fear, anger and joy for {conf.city_name}', fontsize=20)
+    plt.xlabel('Date', fontsize=15)
+    plt.ylabel('Sentiment %', fontsize=15)
+    plt.scatter(meanArr["date"], meanArr["fear"], marker = 'x', label='fear', color = '#FF8800')
+    plt.scatter(meanArr["date"], meanArr["anger"], marker = 'o', label='anger', color = '#009900')
+    plt.scatter(meanArr["date"], meanArr["joy"], marker = '.', label='joy', color = '#0080FF')
+
+    # for emotion in emotions:
+    #     plt.scatter(meanArr["date"], meanArr[emotion], marker = 'x')
+
+    def movingaverage(interval, window_size):
+        window = np.ones(int(window_size))/float(window_size)
+        return np.convolve(interval, window, 'same')
+
+    x_av_fear = movingaverage(meanArr["fear"], 3)
+    x_av_joy = movingaverage(meanArr["joy"], 3)
+    x_av_anger = movingaverage(meanArr["anger"], 3)
+    plt.plot(meanArr["date"], x_av_fear, label='mov. avg. 3D fear', color = '#FF8800')
+    plt.plot(meanArr["date"], x_av_joy, label='mov. avg. 3D joy', color = '#0080FF')
+    plt.plot(meanArr["date"], x_av_anger, label='mov. avg. 3D anger', color = '#009900')
+    plt.legend(loc='upper left')
+    plt.savefig(f"{conf.city_name}_sentiment_fear.png")
+
+
+    # positive / negative sentiment 
+    plt.figure(figsize=(20,8))
+    plt.title(f'Change in positive and negative sentiment for {conf.city_name}', fontsize=20)
+    plt.xlabel('Date', fontsize=15)
+    plt.ylabel('Sentiment %', fontsize=15)
+    plt.scatter(meanArr["date"], meanArr["positive"], marker = 'x', label='positive', color = '#FF8800')
+    plt.scatter(meanArr["date"], meanArr["negative"], marker = 'o', label='negative', color = '#009900')
+
+    # for emotion in emotions:
+    #     plt.scatter(meanArr["date"], meanArr[emotion], marker = 'x')
+
+    x_av_positive = movingaverage(meanArr["positive"], 3)
+    x_av_negative = movingaverage(meanArr["negative"], 3)
+    plt.plot(meanArr["date"], x_av_positive, label='mov. avg. 3D positive', color = '#FF8800')
+    plt.plot(meanArr["date"], x_av_negative, label='mov. avg. 3D negative', color = '#0080FF')
+    plt.legend(loc='upper left')
+    plt.savefig(f"{conf.city_name}_sentiment_posneg.png")
+
+
+    exclude_words = ["http","https","error","RT", "rt"]
+
+    exclude_words.extend(stopcorpus)
+
+    df = pd.concat(files)
+
+    df['wordcloud'] = df['translated_full_text'].astype(str).apply(remove_links)
+
+    df['wordcloud'] = df['wordcloud'].astype(str).apply(lambda x: x.lower())
+
+    df['wordcloud'] = df['wordcloud'].astype(str).apply(remove_apostrophes)
+
+    df['wordcloud'] = df['wordcloud'].astype(str).apply(lambda x: remove_words(x.split(),exclude_words))
+
+    df['wordcloud'] = df['wordcloud'].apply(lambda x: ' '.join(x))
+
+    # display(output_df['wordcloud'])
+
+    wordcloud = WordCloud(width=1920, height=1080).generate(' '.join(df['wordcloud'].astype(str)))
+    wordcloud.to_file(f"{conf.city_name}_wordcloud.png")
 
     # DONE!
     print(f"Success")

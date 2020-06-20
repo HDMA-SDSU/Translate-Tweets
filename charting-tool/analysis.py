37dec072b17313e688388dc5650682399e8ee6e6
Norbert Bonnici <norbert@bonnici.co>
2020-06-20 20:14:49 +0200

    Add city selector

--- charting-tool/analysis.py
+++ charting-tool/analysis.py
@@ -11,6 +11,23 @@ from gooey import Gooey, GooeyParser
 import io
 import sys
 
+cities = [ {'city': "Bari", "italian": "Bari", 'region': "Puglia"}, 
+           {'city': "Bologna", "italian": "Bologna", 'region': "Emilia-Romagna"},
+           {'city': "Cagliari", "italian": "Cagliari", 'region': "Sardegna"},
+           {'city': "Florence", "italian": "Firenze", 'region': "Toscana"},
+           {'city': "Milan", "italian": "Milano", 'region': "Lombardia"},
+           {'city': "Naples", "italian": "Napoli", 'region': "Campania"},
+           {'city': "Palermo", "italian": "Palermo", 'region': "Sicilia"},
+           {'city': "Rome", "italian": "Roma", 'region': "Lazio"},
+           {'city': "Turin", "italian": "Torino", 'region': "Piemonte"},
+           {'city': "Venice", "italian": "Venezia", 'region': "Veneto"}]
+
+def getCities():
+    list_of_cities = []
+    for city in cities:
+        list_of_cities.append(city['city'])
+    return list_of_cities
+
 @Gooey(program_name="COVID-Crowdfight Plotter", progress_regex=r"(\d+)%")
 def parse_args():
     prog_descrip = "Prepare sentiment analysis plots"
@@ -24,19 +41,25 @@ def parse_args():
         widget="DirChooser",
     )
 
+    parser.add_argument(
+        "--run_all",
+        metavar="Run all cities",
+        help="Executes over all 10 cities",
+        action = "store_true",
+        default=False,
+        widget="CheckBox",
+    )
+
+    parser.add_argument(
+        "--city",
+        metavar="Choose city to run",
+        widget="Dropdown",
+        choices=getCities(),
+    )
+
     return parser.parse_args()
 
-cities = ["Bari", "Bologna", "Cagliari", "Florence", "Milan", "Naples", "Palermo", "Rome", "Turin", "Venice"]
-italian_cities = ["Bari", "Bologna", "Cagliari", "Firenze", "Milano", "Napoli", "Palermo", "Roma", "Torino", "Venezia"]
-italian_regions = ["Puglia", "Emilia-Romagna", "Sardegna", "Toscana", "Lombardia", "Campania", "Sicilia", "Lazio", "Piemonte", "Veneto"]
 sentiments = ["fear", "anger", "joy"]
-
-# cities = ["Milan"]
-# italian_cities = ["Milano"]
-# italian_regions = ["Lombardia"]
-# sentiments = ["fear", "anger", "joy"]
-
-counter = 0
 
 def movingaverage(interval, window_size):
         window = np.ones(int(window_size))/float(window_size)
@@ -62,89 +85,88 @@ if __name__ == "__main__":
     conf = parse_args()
 
     for city in cities:
-        print(f"Preparing {city}")
+        if (conf.city == city['city'] or conf.run_all == True):
+            print(f"Preparing {city['city']}")
 
-        directory = f"{conf.datadir}/{city}/sentiment"
+            directory = f"{conf.datadir}/{city['city']}/sentiment"
 
-        files = []
+            files = []
 
-        print(f"Loading data")
+            print(f"Loading data")
 
 
-        for file in os.listdir(directory):
-            filename = os.path.join(directory, file)
-            if filename.endswith(".pickle") or filename.endswith(".pkl"):
-                df = pickle.load(open(filename,'rb'))
-                files.append(df)
-            elif filename.endswith(".xlsx"):
-                df = pd.read_excel(filename)
-                files.append(df)
-        #     else:
-        #         raise NotImplementedError("File type not supported")
+            for file in os.listdir(directory):
+                filename = os.path.join(directory, file)
+                if filename.endswith(".pickle") or filename.endswith(".pkl"):
+                    df = pickle.load(open(filename,'rb'))
+                    files.append(df)
+                elif filename.endswith(".xlsx"):
+                    df = pd.read_excel(filename)
+                    files.append(df)
+            #     else:
+            #         raise NotImplementedError("File type not supported")
 
-        print(f"Data loaded")
+            print(f"Data loaded")
 
-        df = pd.concat(files)
+            df = pd.concat(files)
 
-        df['Datetime'] = pd.to_datetime(df['created_at'])
-        df = df.set_index(['Datetime'])
+            df['Datetime'] = pd.to_datetime(df['created_at'])
+            df = df.set_index(['Datetime'])
 
-        # df.to_pickle(f"{conf.datadir}/{city}/{city}.pickle")
+            # df.to_pickle(f"{conf.datadir}/{city}/{city}.pickle")
 
-        print(f"{city} had {len(df)} tweets")
-        print(df['joy'].resample('M').mean()*100)
-        print(df['fear'].resample('M').mean()*100)
-        print(df['anger'].resample('M').mean()*100)
-        print(df['joy'].resample('Y').mean()*100)
-        print(df['fear'].resample('Y').mean()*100)
-        print(df['anger'].resample('Y').mean()*100)
+            print(f"{city['city']} had {len(df)} tweets")
+            print(df['joy'].resample('M').mean()*100)
+            print(df['fear'].resample('M').mean()*100)
+            print(df['anger'].resample('M').mean()*100)
+            print(df['joy'].resample('Y').mean()*100)
+            print(df['fear'].resample('Y').mean()*100)
+            print(df['anger'].resample('Y').mean()*100)
 
-        filename = f"{conf.datadir}/{city}/{italian_cities[counter]}.csv"
-        citta = pd.read_csv(open(filename,'rb'))
-        citta['DateTime'] = pd.to_datetime(citta['data'])
-        citta = citta.set_index(['DateTime'])
+            filename = f"{conf.datadir}/{city['city']}/{city['italian']}.csv"
+            citta = pd.read_csv(open(filename,'rb'))
+            citta['DateTime'] = pd.to_datetime(citta['data'])
+            citta = citta.set_index(['DateTime'])
 
-        filename2 = f"{conf.datadir}/{city}/{italian_regions[counter]}-death.csv"
-        dead = pd.read_csv(open(filename2,'rb'))
-        dead['DateTime'] = pd.to_datetime(dead['data'])
-        dead = dead.set_index(['DateTime'])
+            filename2 = f"{conf.datadir}/{city['city']}/{city['region']}-death.csv"
+            dead = pd.read_csv(open(filename2,'rb'))
+            dead['DateTime'] = pd.to_datetime(dead['data'])
+            dead = dead.set_index(['DateTime'])
 
-        for sentiment in sentiments:
-            color = 'tab:blue'
-            fig, ax1 = plt.subplots(figsize=(25,15))
-            plt.bar(citta.index, citta.Delta, color = color, alpha=0.6,label=f'Daily new COVID-19 cases')
-            plt.plot(dead.index, dead.Death, color = "black",label=f'Daily fatalities for the {italian_regions[counter]} region', alpha=0.6)
-            plt.fill_between(dead.index, dead.Death, color = "black", alpha=0.6)
-            ax1.set_xlabel('Date', size=13)
-            ax1.set_ylabel('Dailty new COVID-19 cases', color=color, size=13) 
-            start, end = ax1.get_xlim()
-            ax1.xaxis.set_ticks(np.arange(start, end, 0.1))
-            ax1.tick_params(axis='y', labelcolor=color, size=13)
+            for sentiment in sentiments:
+                color = 'tab:blue'
+                fig, ax1 = plt.subplots(figsize=(30,15))
+                plt.bar(citta.index, citta.Delta, color = color, alpha=0.6,label=f'Daily new COVID-19 cases')
+                plt.plot(dead.index, dead.Death, color = "black",label=f'Daily fatalities for the {city["region"]} region', alpha=0.6)
+                plt.fill_between(dead.index, dead.Death, color = "black", alpha=0.6)
+                ax1.set_xlabel('Date', size=13)
+                ax1.set_ylabel('Dailty new COVID-19 cases', color=color, size=13) 
+                start, end = ax1.get_xlim()
+                ax1.xaxis.set_ticks(np.arange(start, end, 0.1))
+                ax1.tick_params(axis='y', labelcolor=color, size=13)
 
-            weekend_indices = find_weekend_indices(citta.index)
-            highlight_datetimes(weekend_indices, ax1, citta)
-            
-            color = 'tab:red'
-            mean = df[sentiment].resample('D').mean()*100
-            ax2 = plt.twinx() 
-            ax2.scatter(mean.index, mean, marker = '.', color = color)
-            x_av = movingaverage(mean, 1)
-            ax2.plot(mean.index, x_av, color = color, label=f'{sentiment}')
-            ax2.set_ylabel(f'{sentiment} (%)', color=color, size=13) 
-            ax2.tick_params(axis='y', labelcolor=color)
-            
-            bbox_props = dict(boxstyle="round", fc="white", alpha=0.8)
+                weekend_indices = find_weekend_indices(citta.index)
+                highlight_datetimes(weekend_indices, ax1, citta)
+                
+                color = 'tab:red'
+                mean = df[sentiment].resample('D').mean()*100
+                ax2 = plt.twinx() 
+                ax2.scatter(mean.index, mean, marker = '.', color = color)
+                x_av = movingaverage(mean, 1)
+                ax2.plot(mean.index, x_av, color = color, label=f'{sentiment}')
+                ax2.set_ylabel(f'{sentiment} (%)', color=color, size=13) 
+                ax2.tick_params(axis='y', labelcolor=color)
+                
+                bbox_props = dict(boxstyle="round", fc="white", alpha=0.8)
 
-            annotations_file = f"{conf.datadir}/{city}.xlsx"
-            a_file = pd.read_excel(open(annotations_file,'rb'))
+                annotations_file = f"{conf.datadir}/{city['city']}.xlsx"
+                a_file = pd.read_excel(open(annotations_file,'rb'))
 
-            for date, row in a_file.iterrows():
-                ax2.annotate(row.Policy, (mdates.date2num(row.Date), mean[row.Date]), xytext=(row.X, row.Y), 
-                            bbox=bbox_props, size=row.Size,textcoords='offset points', arrowprops=dict(arrowstyle='-|>'))
-            
-            ax1.legend(prop={'size': 12}, loc='upper right')
-            ax1.set_title(label=f'Visualizing the effects of {sentiment} in {city} during the time of COVID-19', size=15)
-            fig.savefig(f"{conf.datadir}/{city}/{city}_{sentiment}.png")
-            plt.close(fig)
-
-        counter = counter + 1
+                for date, row in a_file.iterrows():
+                    ax2.annotate(row.Policy, (mdates.date2num(row.Date), mean[row.Date]), xytext=(row.X, row.Y), 
+                                bbox=bbox_props, size=row.Size,textcoords='offset points', arrowprops=dict(arrowstyle='-|>'))
+                
+                ax1.legend(prop={'size': 12}, loc='upper right')
+                ax1.set_title(label=f'Visualizing the effects of {sentiment} in {city["city"]} during the time of COVID-19', size=15)
+                fig.savefig(f"{conf.datadir}/{city['city']}/{city['city']}_{sentiment}.png")
+                plt.close(fig)
